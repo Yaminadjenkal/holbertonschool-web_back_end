86c3ed04b398145ce252e4cb7f6c3807b7ab6ca0
Yaminadjenkal <mina20101951@hotmail.com>
2025-03-27 15:01:28 +0100

    supprimer tout

--- ES6_basic/11-createEmployeesObject.js
+++
@@ -1,6 +0,0 @@
-export default function createEmployeesObject(departmentName, employees) {
-  return {
-    [departmentName]: employees, // Utilisation de noms de propriétés calculées
-  };
-}
-
--- ES6_basic/4-rest-parameter.js
+++
@@ -1,4 +0,0 @@
-// Function to count arguments passed to it using rest parameter
-export default function returnHowManyArguments(...args) {
-  return args.length;
-}
