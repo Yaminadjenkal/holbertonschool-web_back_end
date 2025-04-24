73c2d7c5f5ef64f3c42ae1253d87eb21b8f9b9ac
Yaminadjenkal <mina20101951@hotmail.com>
2025-04-24 22:59:54 +0200

    fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,7 @@
-def add(a: float, b: float) -> float:
-    return a + b
+#!/usr/bin/env python3
+"""Module that contains a single function that adds two integers."""
 
+
+def add(a: float, b: float) -> float:
+    """ Add two integers together."""
+    return a + b
