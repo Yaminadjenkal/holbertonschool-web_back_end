1bbb21226e38f7f59c90846a819066e875b360ff
Yaminadjenkal <mina20101951@hotmail.com>
2026-06-03 12:43:06 +0200

    task 2

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -14,7 +14,7 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """ Initialize the cache """
         super().__init__()
-        self.last_key = None  # Track last inserted key
+        self.order = []  # Track insertion order
 
     def put(self, key, item):
         """ Add an item in the cache using LIFO algorithm.
@@ -24,22 +24,23 @@ class LIFOCache(BaseCaching):
         if key is None or item is None:
             return
 
+        # If key already exists: update value and move it to the end
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            if key in self.order:
+                self.order.remove(key)
+            self.order.append(key)
+            return
+
+        # New key
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Discard last item put in cache (LIFO)
+            last_key = self.order.pop()
+            del self.cache_data[last_key]
+            print("DISCARD: {}".format(last_key))
+
         self.cache_data[key] = item
-        self.last_key = key
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            # Discard the last item put in cache (LIFO)
-            discard_key = self.last_key
-            if discard_key in self.cache_data:
-                del self.cache_data[discard_key]
-                print(f"DISCARD: {discard_key}")
-
-            # After discarding, last_key should be updated to
-            # the new last inserted key if needed
-            if self.cache_data:
-                self.last_key = list(self.cache_data.keys())[-1]
-            else:
-                self.last_key = None
+        self.order.append(key)
 
     def get(self, key):
         """ Retrieve an item by key.
@@ -48,3 +49,4 @@ class LIFOCache(BaseCaching):
         if key is None:
             return None
         return self.cache_data.get(key)
+
