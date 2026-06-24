aa8284709998f0643a9227bb75711be7ac990e5f
Yaminadjenkal <mina20101951@hotmail.com>
2026-06-24 23:42:40 +0200

    task 1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,4 +1,14 @@
-from user import User
+#!/usr/bin/env python3
+"""
+DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from .user import Base, User
+
 
 class DB:
     """DB class
