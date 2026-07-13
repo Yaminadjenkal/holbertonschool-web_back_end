3bbe4cc8cb087e2fbcb259c12272674744538c77
Yaminadjenkal <mina20101951@hotmail.com>
2026-07-13 02:26:37 +0200

    test

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -33,24 +33,4 @@ class Cache:
     def get_int(self, key: str) -> Optional[int]:
         """Retrieve an integer"""
         return self.get(key, fn=int)
-#!/usr/bin/env python3
-"""
-Exercise: Writing strings to Redis
-"""
-import redis
-import uuid
-from typing import Union
-
-
-class Cache:
-    def __init__(self):
-        """Initialize Redis client and flush database"""
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Store data in Redis with a random UUID key"""
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
 
