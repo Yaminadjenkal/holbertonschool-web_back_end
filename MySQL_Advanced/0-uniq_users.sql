de500e450a374caa69f54b753133781c32843da5
Yaminadjenkal <mina20101951@hotmail.com>
2026-07-18 22:51:09 +0200

    tets

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
+-- Task 0: Create users table with unique email constraint
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id    INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name  VARCHAR(255)
 );
- 
